58f0de5e798ba566714805743a95cd1a80517f5d
narvas12 <ebukaemmanuel71@gmail.com>
2023-11-09 04:06:44 -0500

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+mobileMenu = document.querySelector('.mobile')
+
+mobileMenu.onclick = function () {
+    navBar = document.querySelector('.nav_bar')
+    navBar.classList.toggle('active')
+}
+
+
+
+const textElement = document.getElementById("typed-text");
+const text = textElement.textContent;
+textElement.textContent = ""; // Clear the text initially
+
+let i = 0;
+function typeText() {
+    if (i < text.length) {
+        textElement.textContent += text.charAt(i);
+        i++;
+        setTimeout(typeText, 50); // Adjust the speed by changing the timeout value (milliseconds)
+    }
+}
+
+typeText(); // Start the typing animation
+
+window.addEventListener('scroll', () => {
+    console.log('scrolled')
+    document.querySelector('header').classList.toggle('window-scroll', window.scrollY > 0)
+})
